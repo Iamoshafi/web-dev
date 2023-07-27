99f3b2fc828005e26f7f2a621254ce78513b6932
Omotiafe <omotiafearegbeyen@gmail.com>
2023-07-27 11:35:22 +0100

    final commit

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-"Use strict";
+'Use strict';
 
-const content = document.querySelector(".content");
-const card = document.querySelector(".card");
-const button = document.querySelector(".button");
-const button2 = document.querySelector(".button2");
-const input = document.querySelector(".input");
-const error = document.querySelector(".error");
+const content = document.querySelector('.content');
+const card = document.querySelector('.card');
+const button = document.querySelector('.button');
+const button2 = document.querySelector('.button2');
+const input = document.querySelector('.input');
+const error = document.querySelector('.error');
 let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-button.addEventListener("click", function () {
-  if (input.value === "") {
-    error.textContent = "Email Required!";
+button.addEventListener('click', function () {
+  if (input.value === '') {
+    error.textContent = 'Email Required!';
   } else if (!input.value.match(mailFormat)) {
-    error.textContent = "Valid Email Required!";
+    error.textContent = 'Valid Email Required!';
   } else {
-    card.classList.toggle("hidden");
-    content.classList.toggle("hidden");
+    card.classList.toggle('hidden');
+    content.classList.toggle('hidden');
   }
 });
 
-button2.addEventListener("click", function () {
-  card.classList.toggle("hidden");
-  content.classList.toggle("hidden");
-  error.textContent = "";
-  input.value = "";
+button2.addEventListener('click', function () {
+  card.classList.toggle('hidden');
+  content.classList.toggle('hidden');
+  error.textContent = '';
+  input.value = '';
 });
